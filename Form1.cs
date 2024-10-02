c3a9cf34630a808fed4eda2594371094c2903a95
vvwvvwwvvw <wjddnr3304@gmail.com>
2024-10-02 09:17:23 +0900

    멀티 쓰레드 예제

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,62 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+
+            //Load 이벤트 선언
+            this.Load += FormLoad_Event;
+        }
+        /// <summary>
+        /// Form 로드 이벤트 핸들러
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormLoad_Event(object sender, EventArgs e)
+        {
+            // 스레드 선언
+            Thread aThread = new Thread(new ThreadStart(A));
+            Thread bThread = new Thread(new ThreadStart(B));
+            Thread cThread = new Thread(new ThreadStart(C));
+
+            // 스레드 시작
+            aThread.Start();
+            bThread.Start();
+            cThread.Start();
+        }
+        private void A()
+        {
+            CrossThread(textBox1, 100, "A");
+        }
+        private void B()
+        {
+            CrossThread(textBox1, 20, "B");
+        }
+        private void C()
+        {
+           CrossThread(textBox1, 100, "C");
+        }
+        /// <summary>
+        /// UI 쓰레드와의 충돌로 인해
+        /// 크로스 스레드 방지 메서드
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="idx"></param>
+        /// <param name="text"></param>
+        private void CrossThread(Control item, int idx, string text)
+        {
+            if (item.InvokeRequired)
+            {
+                item.BeginInvoke(new MethodInvoker(delegate ()
+                {
+                    for (int i = 0; i < idx; i++)
+                    {
+                        item.Text += text;
+                    }
+                }));
+            }
         }
     }
 }
